8c6a57f82a6d3521f1b6b1dd9ec4a575a4f68ec3
Raksha Shanbhag <rakcollegeadm@gmail.com>
2022-09-21 08:14:56 -0400

    upload infrastructure files

---
+++ RASVPtracker.Service/DIExtensions/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using RSVPtracker.Core.Interfaces;
+using RSVPtracker.Infrastructure.Repositories;
+
+namespace RSVPtracker.Infrastructure.DIExtensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IDatabaseInitialiser, DatabaseInitialiser>();
+
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"], b => b.MigrationsAssembly("RSVP-tracker")));
+
+            return services;
+        }
+    }
+}
