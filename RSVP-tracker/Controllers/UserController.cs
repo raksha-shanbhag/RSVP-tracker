a14d33c492cb7d31bcc7a400abcbc5e28a776031
Raksha Shanbhag <rakcollegeadm@gmail.com>
2022-09-21 07:44:40 -0400

    Add files via upload

---
+++ RSVP-tracker/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RSVP_tracker.Services;
+
+namespace RSVP_tracker.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        public readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var userList = await _userService.GetAllUsers();
+            return Ok(userList);
+        }
+
+     }
+}
