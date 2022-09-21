088dbe92302ad46c5ea2b48662697e957719f375
Raksha Shanbhag <rakcollegeadm@gmail.com>
2022-09-21 08:13:49 -0400

    upload core

---
+++ RSVPtracker.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSVPtracker.Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> Get(int id);
+        Task<IEnumerable<T>> GetAll();
+        Task Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
